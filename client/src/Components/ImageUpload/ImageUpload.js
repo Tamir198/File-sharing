4e35e92ca2cff257e91ba43a10ef609959a6c214
Tamir <1tamir198@gmail.com>
2021-12-05 00:08:14 +0200

    Saving images on server

--- client/src/Components/ImageUpload/ImageUpload.js
+++ client/src/Components/ImageUpload/ImageUpload.js
@@ -13,20 +13,16 @@ function ImageUpload(){
     const formData = new FormData();
 
 		formData.append('selected-file', selectedFile);
-    console.log(selectedFile);
 
-    //TODo replace this with another
 		fetch(
-			"http://localhost:8081/" + selectedFile.name,
+			"http://localhost:8081/v1/file",
 			{
 				method: 'POST',
 				body: formData,
+        headers: new Headers({'content-type': 'multipart/form-data'}),
 			}
 		)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
+			.then((response) =>console.log(response))
 			.catch((error) => {
 				console.error('Error:', error);
 			});
@@ -35,7 +31,7 @@ function ImageUpload(){
 
 	return(
    <div>
-			<input type="file" name="file" onChange={changeHandler} />
+			<input type="file" name="image" onChange={changeHandler} />
 			{isFilePicked ? (
 				<div>
           <img src={selectedFile} alt="This is your file" />
--- server.js
+++ server.js
@@ -4,33 +4,32 @@ var app = express();
 const cors = require('cors');
 const fs = require('fs');
 const multer = require("multer");
-const path = require('path');
+const upload = multer({dest:'uploads/'});
 
-app.use(express.static('public'))
 app.use(cors());
 
 
-//TODo save image to directory
-app.put('/v1/:fileName', (req, res) =>{
-  //saveFile();
- // fs.appendFile(req.params.fileName, req.body);
-  res.send(`http://localhost:8081/${req.params.fileName}`);
- });
-
-
- app.post('http://localhost:8081/', (req, res) =>{
-  res.send("Post was sent");
- });
-  
+// //send back image for the url `http://localhost:8081/fileName`
+// app.post('/v1/file', (req, res) =>{
+//   console.log(req);
+// //  res.send(`http://localhost:8081/${req.params.fileName}`);
+//  res.send("http://localhost:8081/home.png");
+// });
 
 //send back image for the url `http://localhost:8081/fileName`
-app.post('http://localhost:8081/:fileName', (req, res) =>{
- res.send(`http://localhost:8081/${req.params.fileName}`);
+app.post('/v1/file',upload.single("image"), (req, res) =>{
+  console.log(req.file);
+ res.send("http://localhost:8081/home.png");
 });
 
-var server = app.listen(8081, function () {
+
+app.get('/:fileName', (req, res) =>{
+  res.sendFile(`http://localhost:8081/home.png`);
+ });
+
+var server = app.listen(8081, () => {
    var host = server.address().address
    var port = server.address().port
-   
+
    console.log("Example app listening at http://%s:%s", host, port)
 })
